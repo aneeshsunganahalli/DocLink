86afcb2425031a904a1ba696efbda632c886bb41
Aneesh <aneeshappid@gmail.com>
2025-01-26 20:35:21 +0530

    Completed Appointment UI

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -5,9 +5,12 @@ import { doctors } from "../assets/assets_frontend/assets";
 export const AppContext = createContext();
 
 export default function AppContextProvider(props) {
+
+  const currencySymbol = '$'
   
   const value = {
-    doctors
+    doctors,
+    currencySymbol
   }
 
   return (
