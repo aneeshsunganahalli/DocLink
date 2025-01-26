1e7f843bf040529dbf57e3e54caadfc4def17f12
Aneesh <aneeshappid@gmail.com>
2025-01-27 00:52:33 +0530

    Finished Profile Page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -18,11 +18,12 @@ export default function Login() {
         <div className='flex flex-col w-3/8 gap-4 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-zinc-600 text-sm shadow-lg'>
           <p className='text-2xl font-semibold'> {state === 'Sign Up'? "Create Account" : "Login"} </p>
           <p>Please {state === 'Sign Up'? "sign up" : "log in"} to book an appoitment</p>
-
-          <div className='w-full'>
+          {
+            state === 'Sign Up' && <div className='w-full'>
             <p>Full Name</p>
             <input className='border border-zinc-300 rounded w-full p-2 mt-1' type='text' onChange={(e) => setName(e.target.name)} value={name} required/>
           </div>
+          }
 
           <div className='w-full'>
             <p>Email</p>
@@ -37,8 +38,8 @@ export default function Login() {
           <button className='bg-primary mt-2 text-white w-full py-2 rounded-md text-base'> {state === 'Sign Up'? "Create Account" : "Login"} </button>
           {
             state === 'Sign Up'
-            ? <p>Already have an account? <span className='text-primary underline cursor-pointer'>Login here</span></p>
-            : <p>Create a new account <span className='text-primary underline cursor-pointer'>Click here</span></p>
+            ? <p>Already have an account? <span onClick={() => setState('Login')} className='text-primary underline cursor-pointer'>Login here</span></p>
+            : <p>Create a new account <span onClick={() => setState('Sign Up')} className='text-primary underline cursor-pointer'>Click here</span></p>
           }
         </div>
       </form>
