5306580bfa9a0e06975210d88e2ab1c7cc0a1fef
Aneesh <aneeshappid@gmail.com>
2025-01-27 17:17:17 +0530

    Set Up Backend Folder and Connected to Database

---
+++ backend/models/doctor.model.js
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+
+const doctorSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  }
+})
---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import connectDB from './config/mongodb.js';
+import connectCloudinary from './config/cloudinary.js';
+
+const app = express();
+const port = process.env.port || 4000
+connectDB();
+connectCloudinary();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req,res) => {
+  res.send('API WORKING')
+})
+
+app.listen(port, () => console.log("Server Started",port))
