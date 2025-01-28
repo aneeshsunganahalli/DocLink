b8e8a9f3857ffb446390ca1a6e62d183b596403a
Aneesh <aneeshappid@gmail.com>
2025-01-28 23:01:31 +0530

    Finished doctor addition UI

--- admin/src/pages/Admin/AddDoctor.jsx
+++ admin/src/pages/Admin/AddDoctor.jsx
@@ -1,9 +1,97 @@
 import React from 'react'
+import { assets } from '../../assets/assets_admin/assets'
 
 export default function AddDoctor() {
   return (
-    <div>
-      
-    </div>
+    <form className='w-full m-5'>
+      <p className='mb-3 text-lg font-medium'>Add Doctor</p>
+
+      <div className='bg-white px-8 py-8 border-none shadow-lg rounded-2xl w-full max-w-4xl max-h-[80vh] overflow-y-scroll'>
+
+        <div className='flex items-center gap-4 mb-8 text-gray-500'>
+          <label htmlFor='doc-img'>
+            <img className='w-16 bg-gray-100 rounded-full cursor-pointer' src={assets.upload_area} alt="" />
+          </label>
+          <input type='file' id='doc-img' hidden />
+          <p>Upload doctor <br /> picture</p>
+        </div>
+
+        <div className='flex flex-col lg:flex-row items-start gap-10 text-gray-600'>
+          <div className='w-full lg:flex-1 flex flex-col gap-4'>
+
+            <div className='flex-1 flex flex-col gap-1'>
+              <p>Doctor Name</p>
+              <input className='border rounded px-3 py-2' type='text' placeholder='Name' required />
+            </div>
+
+            <div className='flex-1 flex flex-col gap-1'>
+              <p>Doctor Email</p>
+              <input className='border rounded px-3 py-2' type='email' placeholder='Email' required />
+            </div>
+
+            <div className='flex-1 flex flex-col gap-1'>
+              <p>Doctor Password</p>
+              <input className='border rounded px-3 py-2' type='password' placeholder='Password' required />
+            </div>
+
+            <div className='flex-1 flex flex-col gap-1'>
+              <p>Experience</p>
+              <select className='border rounded px-3 py-2'>
+                <option value="1 Year">1 Year</option>
+                <option value="2 Year">2 Years</option>
+                <option value="3 Year">3 Years</option>
+                <option value="4 Year">4 Years</option>
+                <option value="5 Year">5 Years</option>
+                <option value="6 Year">6 Years</option>
+                <option value="7 Year">7 Years</option>
+                <option value="8 Year">8 Years</option>
+                <option value="9 Year">9 Years</option>
+                <option value="10 Year">10 Years</option>
+              </select>
+            </div>
+
+            <div className='flex-1 flex flex-col gap-1'>
+              <p>Fees</p>
+              <input className='border rounded px-3 py-2' type='number' placeholder='fees' required />
+            </div>
+          </div>
+
+          <div className='w-full lg:flex-1 flex flex-col gap-4 '>
+            <div className='flex-1 flex flex-col gap-1'>
+              <p>Speciality</p>
+              <select className='border rounded px-3 py-2'>
+                <option value="General Physician">General Physician</option>
+                <option value="Gynecologist">Gynecologist</option>
+                <option value="Dermatologist">Dermatologist</option>
+                <option value="Pediatricians">Pediatricians</option>
+                <option value="Neurologist">Neurologist</option>
+                <option value="Gastroenterologist">Gastroenterologist</option>
+              </select>
+            </div>
+
+            <div className='flex-1 flex flex-col gap-1'>
+              <p>Education</p>
+              <input className='border rounded px-3 py-2' type='text' placeholder='Education' required />
+            </div>
+
+            <div className='flex-1 flex flex-col gap-1'>
+              <p>Address</p>
+              <input className='border rounded px-3 py-2' type='text' placeholder='Address 1' required />
+              <input className='border rounded px-3 py-2' type='text' placeholder='Address 2' required />
+            </div>
+
+          </div>
+        </div>
+
+        <div>
+          <p className='mt-4 mb-2'>About Doctor</p>
+          <textarea className='w-full px-4 pt-2 border rounded' placeholder='Write about the doctor' rows={5} required />
+        </div>
+
+        <button className=' mt-3 bg-primary px-10 py-3 text-white rounded-full hover:scale-105 transition-all duration-300'>
+          Add Doctor
+        </button>
+      </div>
+    </form>
   )
 }
