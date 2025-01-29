2a33e895007956e875c0b0619558d367ae9a4316
Aneesh <aneeshappid@gmail.com>
2025-01-30 00:40:10 +0530

    Completed Admin Dashboard

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -7,32 +7,33 @@ export const AdminContext = createContext()
 
 const AdminContextProvider = (props) => {
 
-  const [aToken,setAToken] = useState(localStorage.getItem('aToken') ? localStorage.getItem('aToken') : '')
-  const [doctors,setDoctors] = useState([])
+  const [aToken, setAToken] = useState(localStorage.getItem('aToken') ? localStorage.getItem('aToken') : '')
+  const [doctors, setDoctors] = useState([])
   const [appointments, setAppointments] = useState([])
+  const [dashData, setDashData] = useState(false)
 
   const backendUrl = import.meta.env.VITE_BACKEND_URL
 
   const getAllDoctors = async () => {
     try {
 
-      const {data} = await axios.post(backendUrl + '/api/admin/all-doctors', {}, {headers: {aToken}})
+      const { data } = await axios.post(backendUrl + '/api/admin/all-doctors', {}, { headers: { aToken } })
       if (data.success) {
         setDoctors(data.doctors)
         console.log(data.doctors);
-        
+
       } else {
-        toast.error(data.message)  
+        toast.error(data.message)
       }
 
     } catch (error) {
-        toast.error(error.message)
+      toast.error(error.message)
     }
   }
 
   const changeAvailability = async (docId) => {
     try {
-      const {data} = await axios.post(backendUrl + '/api/admin/change-availability', {docId}, {headers:{aToken}})
+      const { data } = await axios.post(backendUrl + '/api/admin/change-availability', { docId }, { headers: { aToken } })
       if (data.success) {
         toast.success(data.message)
         getAllDoctors()
@@ -46,11 +47,11 @@ const AdminContextProvider = (props) => {
 
   const getAllAppointments = async () => {
     try {
-      const {data} = await axios.get(backendUrl + "/api/admin/appointments", {headers:{aToken}})
+      const { data } = await axios.get(backendUrl + "/api/admin/appointments", { headers: { aToken } })
       if (data.success) {
         setAppointments(data.appointments)
         console.log(data.appointments);
-        
+
       } else {
         toast.error(data.message)
       }
@@ -61,7 +62,7 @@ const AdminContextProvider = (props) => {
 
   const cancelAppointment = async (appointmentId) => {
     try {
-      const {data} = await axios.post(backendUrl + '/api/admin/cancel-appointment', {appointmentId}, {headers:{aToken}})
+      const { data } = await axios.post(backendUrl + '/api/admin/cancel-appointment', { appointmentId }, { headers: { aToken } })
       if (data.success) {
         toast.success(data.message)
         getAllAppointments()
@@ -73,12 +74,27 @@ const AdminContextProvider = (props) => {
     }
   }
 
+  const getDashData = async () => {
+    try {
+      const { data } = await axios.get(backendUrl + '/api/admin/dashboard', { headers: { aToken } })
+      if (data.success) {
+        setDashData(data.dashData)
+        console.log(data.dashData);
+
+      } else {
+        toast.error(data.message)
+      }
+    } catch (error) {
+      toast.error(error.message)
+    }
+  }
+
   const value = {
-    aToken,setAToken,
-    backendUrl,doctors,
-    getAllDoctors,changeAvailability,
-    getAllAppointments,setAppointments,appointments,
-    cancelAppointment
+    aToken, setAToken,
+    backendUrl, doctors,
+    getAllDoctors, changeAvailability,
+    getAllAppointments, setAppointments, appointments,
+    cancelAppointment, dashData, getDashData
   }
 
   return (
--- backend/routes/admin.route.js
+++ backend/routes/admin.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addDoctor, allDoctors, appointmentCancel, appointmentsAdmin, loginAdmin } from "../controllers/admin.controller.js";
+import { addDoctor, adminDashboard, allDoctors, appointmentCancel, appointmentsAdmin, loginAdmin } from "../controllers/admin.controller.js";
 import upload from "../middlewares/multer.js";
 import authAdmin from "../middlewares/authAdmin.js";
 import { changeAvailabilty } from "../controllers/doctor.controller.js";
@@ -12,5 +12,6 @@ adminRouter.post('/all-doctors',authAdmin, allDoctors)
 adminRouter.post('/change-availability',authAdmin, changeAvailabilty)
 adminRouter.post('/cancel-appointment',authAdmin, appointmentCancel)
 adminRouter.get('/appointments',authAdmin, appointmentsAdmin)
+adminRouter.get('/dashboard',authAdmin, adminDashboard)
 
 export default adminRouter
