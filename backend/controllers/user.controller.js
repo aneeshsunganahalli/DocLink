bb1d6b7f59a172b13a2780175a85df3f540d9f5c
Aneesh <aneeshappid@gmail.com>
2025-01-29 21:25:37 +0530

    Fetched User Appointments from Database

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -171,4 +171,18 @@ const bookAppointment = async (req,res) => {
   }
 }
 
-export {registerUser, loginUser, getProfile, updateProfile, bookAppointment}
+//API to get User Appointments
+const listAppointment= async (req,res) => {
+  try {
+    const {userId} = req.body
+    const appointments = await appointmentModel.find({userId})
+
+    res.json({success:true,appointments})
+
+  } catch (error) {
+    console.log(error)
+    res.json({success:true,message:error.message})
+  }
+}
+
+export {registerUser, loginUser, getProfile, updateProfile, bookAppointment, listAppointment}
