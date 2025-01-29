211ad07f687adf95f3db6712e2f038a296464927
Aneesh <aneeshappid@gmail.com>
2025-01-29 22:26:21 +0530

    Implemented Cancel Appointment function

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -185,4 +185,40 @@ const listAppointment= async (req,res) => {
   }
 }
 
-export {registerUser, loginUser, getProfile, updateProfile, bookAppointment, listAppointment}
+//API to cancel appointment
+const cancelAppointment = async (req,res) => {
+  try {
+    const {userId, appointmentId} = req.body
+    const appointmentData = await appointmentModel.findById(appointmentId)
+
+    //Verify Appointment
+    if (appointmentData.userId !== userId) {
+      return res.json({success:false, message: "Unauthorized Action"})
+    }
+
+    await appointmentModel.findByIdAndUpdate(appointmentId,{cancelled:true})
+
+    //Releasing a Slot
+    const {docId, slotDate, slotTime} = appointmentData
+
+    const doctorData = await doctorModel.findById(docId)
+
+    if (!doctorData) {
+      return res.json({ success: false, message: "Doctor not found" });
+    }
+
+    let slots_booked = doctorData.slots_booked
+
+    slots_booked[slotDate] = slots_booked[slotDate].filter(e => e !== slotTime)
+
+    await doctorModel.findByIdAndUpdate(docId, {slots_booked})
+
+    res.json({success:true,message:"Appointment Cancelled"})
+
+  } catch (error) {
+    console.log(error)
+    res.json({success:false,message:error.message})
+  }
+}
+
+export {registerUser, loginUser, getProfile, updateProfile, bookAppointment, listAppointment, cancelAppointment}
--- client/src/pages/MyAppointments.jsx
+++ client/src/pages/MyAppointments.jsx
@@ -5,10 +5,10 @@ import { toast } from 'react-toastify'
 
 export default function MyAppointments() {
 
-  const {backendUrl, token} = useContext(AppContext)
+  const { backendUrl, token, getDoctorsData } = useContext(AppContext)
 
-  const [appointments,setAppointments] = useState([])
-  const months = ["","Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+  const [appointments, setAppointments] = useState([])
+  const months = ["", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
   const slotDateFormat = (slotDate) => {
     const dateArray = slotDate.split('_')
@@ -18,25 +18,42 @@ export default function MyAppointments() {
 
   const getUserAppointments = async () => {
     try {
-      
-      const {data} = await axios.get(backendUrl + '/api/user/appointments', {headers:{token}})
 
-      if (data.success){
+      const { data } = await axios.get(backendUrl + '/api/user/appointments', { headers: { token } })
+
+      if (data.success) {
         setAppointments(data.appointments.reverse())
         console.log(data.appointments)
       }
 
     } catch (error) {
       console.log(error);
-      toast.error(error.message) 
+      toast.error(error.message)
+    }
+  }
+
+  const cancelAppointment = async (appointmentId) => {
+    try {
+      const { data } = await axios.post(backendUrl + '/api/user/cancel-appointment', { appointmentId }, { headers: { token } })
+      if (data.success) {
+        toast.success(data.message)
+        getUserAppointments()
+        getDoctorsData()
+      } else {
+        toast.error(data.message)
+      }
+
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message)
     }
   }
 
   useEffect(() => {
-    if(token) {
+    if (token) {
       getUserAppointments()
     }
-  },[token])
+  }, [token])
 
   return (
     <div>
@@ -44,7 +61,7 @@ export default function MyAppointments() {
 
       <div>
         {
-          appointments.map((item,index) => (
+          appointments.map((item, index) => (
             <div className='grid grid-cols-[1fr_2fr] gap-4 sm:flex sm:gap-6 py-2 border-b' key={index}>
               <div>
                 <img className='w-32 bg-indigo-50' src={item.docData.image} alt="" />
@@ -61,8 +78,9 @@ export default function MyAppointments() {
               <div></div>
 
               <div className='flex flex-col gap-2 justify-end'>
-                <button className='text-sm rounded text-center sm:min-w-48 py-2 border hover:bg-primary hover:text-white transition-all duration-300 hover:translate-y-[-3px]'>Pay Online</button>
-                <button className='text-sm  rounded text-center sm:min-w-48 py-2 border hover:bg-red-600 hover:text-white transition-all duration-300 hover:translate-y-[-3px]'>Cancel appointment</button>
+                {!item.cancelled && <button className='text-sm rounded text-center sm:min-w-48 py-2 border hover:bg-primary hover:text-white transition-all duration-300 hover:translate-y-[-3px]'>Pay Online</button>}
+                {!item.cancelled && <button onClick={() => cancelAppointment(item._id)} className='text-sm  rounded text-center sm:min-w-48 py-2 border hover:bg-red-600 hover:text-white transition-all duration-300 hover:translate-y-[-3px]'>Cancel appointment</button>}
+                {item.cancelled && <button className='sm:min-w-48 py-2 border border-red-500 rounded text-red-500'>Appointment cancelled</button>}
               </div>
             </div>
           ))
